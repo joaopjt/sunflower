b1ec374eab23a9e2d7713d20cf9bcb1c53e39d1d
João Pedro <joaopjt@icloud.com>
2023-09-14 20:47:14 -0300

    added b() method values

--- src/index.js
+++ src/index.js
@@ -140,7 +140,7 @@ class Sunflower {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['bow'],
       web: []
     };
     
@@ -156,19 +156,19 @@ class Sunflower {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['corpse'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['people'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['fire'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['ten'];
           break;
       }
     }
