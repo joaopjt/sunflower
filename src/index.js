a376713d37fcb8d1915df8fa797d7c997d2d2998
João Pedro <joaopjt@icloud.com>
2023-09-14 21:58:02 -0300

    added m() method value

--- src/index.js
+++ src/index.js
@@ -576,7 +576,7 @@ class Sunflower {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['twenty'],
       web: []
     };
     
@@ -592,19 +592,19 @@ class Sunflower {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['earth'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['heart'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['disaster'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['bow'];
           break;
       }
     }
